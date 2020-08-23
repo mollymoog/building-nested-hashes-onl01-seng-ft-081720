de36c441004a2358eac567cdb30f0ecfe59c3bf8
molly mk <mmkover@gmail.com>
2020-08-23 15:04:01 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -5,7 +5,16 @@ def fourth_challenge
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
-        #your hashes here!
+        :friend_1 => {
+          name: "Benvolio",
+          age: "17",
+          attitude: "worried"
+        },
+        :friend_2 => {
+          name: "Mercutio",
+          age: "18",
+          attitude: "hot-headed"
+        }
       ]
    }, 
    :capulet => {
